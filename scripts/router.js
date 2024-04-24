c068f23edaea6e343c2cb1d627b7965f81c8b0aa
Gianmarco Biscini <g.biscini@gmail.com>
2024-04-24 11:00:11 +0200

    metadata

--- scripts/router.js
+++ scripts/router.js
@@ -5,13 +5,40 @@ window.addEventListener('load', () => {
         window.history.pushState({}, pathname, window.location.origin + pathname);
         router();
     };
-
-    const routes = {
-        '/': 'home.html',
-        '/timo': 'timo.html',
-        '/vento':'vento.html',
-        '/fede':'fede.html',
-    };
+    const parseDirectoryListing= async () => {
+        // Get all the <li> elements in the HTML document
+        let dirListingHTML=await fetch("metadata/articles.html").then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            console.log("done")
+            return response.text();
+        });
+        const listItems = dirListingHTML.querySelectorAll('ul li');
+        const resultList = [];
+    
+        // Loop through each <li> element
+        listItems.forEach(item => {
+            const src = item.querySelector('.src').textContent;
+            const file = item.querySelector('.file').textContent;
+            const date = item.querySelector('.date').textContent;
+    
+            // Construct an object with the data from the <span> elements
+            const dataObject = {
+                src: src,
+                file: file,
+                date: date
+            };
+    
+            // Add the object to the resultList array
+            resultList.push(dataObject);
+        });
+    
+        // Return the resultList array containing all the data objects
+        return resultList;
+    }
+    
+    const routes = parseDirectoryListing();
 
     const router = async () => {
     const path = window.location.pathname;
