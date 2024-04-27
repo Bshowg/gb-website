95a57e98c5d14dac7f1be267a2308164cb92b62f
Gianmarco Biscini <g.biscini@gmail.com>
2024-04-27 09:45:55 +0200

    styling

--- scripts/router.js
+++ scripts/router.js
@@ -5,44 +5,6 @@ window.addEventListener('load', async () => {
         window.history.pushState({}, pathname, window.location.origin + pathname);
         router();
     };
-    const parseDirectoryListing= async () => {
-        // Get all the <li> elements in the HTML document
-        let dirListingHTML=await fetch("./metadata/articles.html").then(response => {
-            
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            console.log("done")
-            return response.text();
-        }).then(function(html) {
-            // Initialize the DOM parser
-            var parser = new DOMParser();
-    
-            // Parse the text
-            var doc = parser.parseFromString(html, "text/html");
-    
-            // You can now even select part of that html as you would in the regular DOM 
-            // Example:
-            // var docArticle = doc.querySelector('article').innerHTML;
-    
-            return doc;
-        });
-        console.log(dirListingHTML);
-        const listItems = dirListingHTML.querySelectorAll('ul li');
-        const routes = {};
-    
-        // Loop through each <li> element
-        listItems.forEach(item => {
-            const route = item.querySelector('.src').textContent;
-            const path = item.querySelector('.file').textContent;
-    
-            // Construct an object with the data from the <span> elements
-            routes[route]=path;
-        });
-    
-        // Return the resultList array containing all the data objects
-        return routes;
-    }
     async function getArticlesPhp() {
     
         let json=await fetch("./php/get_articles.php").then(response => {
