ab6e2cb54860e8283d013c8ca6f2a09055efdc1b
Gianmarco Biscini <g.biscini@gmail.com>
2024-04-29 17:49:25 +0200

    donut

--- scripts/anglicismi1.js
+++ scripts/anglicismi1.js
@@ -2,10 +2,12 @@
 if (document.readyState === "loading") {
     document.addEventListener("DOMContentLoaded", (event) => {
         barAnglicismi();
+        addNeologismi();
     });
 } else {
     // DOMContentLoaded has already fired
     barAnglicismi();
+    addNeologismi();
 }
 
   function barAnglicismi(){
@@ -51,4 +53,50 @@ svg.append("g")
     .attr("height", 80 )
     .style("margin-top", "40px")
     .attr("fill", "white")
+  }
+
+  function addNeologismi(){
+    var width = 400
+    height = 400
+    margin = 10
+
+// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+var radius = Math.min(width, height) / 2 - margin
+
+// append the svg object to the div called 'donutNeologismi'
+var svg = d3.select("#donutNeologismi")
+  .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+// Create dummy data
+var data = {crudi:51.7,altro:48.3}
+
+// set the color scale
+var color = d3.scaleOrdinal()
+  .domain(data)
+  .range(["white","grey" ])
+
+// Compute the position of each group on the pie:
+var pie = d3.pie()
+  .value(function(d) {return d.value; })
+var data_ready = pie(d3.entries(data))
+
+// Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+svg
+  .selectAll('whatever')
+  .data(data_ready)
+  .enter()
+  .append('path')
+  .attr('d', d3.arc()
+    .innerRadius(100)         // This is the size of the donut hole
+    .outerRadius(radius)
+  )
+  .attr('fill', function(d){ return(color(d.data.key)) })
+  .attr("stroke", "black")
+  .style("stroke-width", "2px")
+  .style("opacity", 0.7)
+
   }
