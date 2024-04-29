e500729ca1d7dfd3d0eacdf272158330d38771b2
Gianmarco Biscini <g.biscini@gmail.com>
2024-04-29 17:30:36 +0200

    commit

--- scripts/anglicismi1.js
+++ scripts/anglicismi1.js
@@ -11,49 +11,41 @@ if (document.readyState === "loading") {
   function barAnglicismi(){
     console.log("eccomi")
     const data=[{anno:"1990",numero:1700},{anno:"2017",numero:3400},{anno:"2020",numero:3958}]
-    const svg = d3.select("#barDevotoOli"),
-    width = +svg.style("width").replace("px", ""),
-    height = +svg.style("height").replace("px", "");
+    var margin = {top: 20, right: 30, bottom: 40, left: 90},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+    const svg = d3.select("#barDevotoOli").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
       // Add X axis
-      d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv", function(data) {
+      var x = d3.scaleLinear()
+      .domain([0, d3.max(data, function(d) { return d.numero; })])
+      .range([0, width]);
+svg.append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(x))
+  .selectAll("text")
+    .attr("transform", "translate(-10,0)rotate(-45)")
+    .style("text-anchor", "end");
 
-  // Add X axis
-  var x = d3.scaleLinear()
-    .domain([0, 13000])
-    .range([ 0, width]);
-  svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x))
-    .selectAll("text")
-      .attr("transform", "translate(-10,0)rotate(-45)")
-      .style("text-anchor", "end");
+// Y axis
+var y = d3.scaleBand()
+  .range([ 0, height ])
+  .domain(data.map(function(d) { return d.anno; }))
+svg.append("g")
+  .call(d3.axisLeft(y))
 
-  // Y axis
-  var y = d3.scaleBand()
-    .range([ 0, height ])
-    .domain(data.map(function(d) { return d.Country; }))
-    .padding(.1);
-  svg.append("g")
-    .call(d3.axisLeft(y))
-
-  //Bars
   svg.selectAll("myRect")
     .data(data)
     .enter()
     .append("rect")
     .attr("x", x(0) )
-    .attr("y", function(d) { return y(d.Country); })
-    .attr("width", function(d) { return x(d.Value); })
+    .attr("y", function(d) { return y(d.anno); })
+    .attr("width", function(d) { return x(d.numero); })
     .attr("height", y.bandwidth() )
-    .attr("fill", "#69b3a2")
-
-
-    // .attr("x", function(d) { return x(d.Country); })
-    // .attr("y", function(d) { return y(d.Value); })
-    // .attr("width", x.bandwidth())
-    // .attr("height", function(d) { return height - y(d.Value); })
-    // .attr("fill", "#69b3a2")
-
-})
+    .attr("fill", "white")
   }
