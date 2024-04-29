2851e6c14261f5a84d772620f2b177ffda61c9db
Gianmarco Biscini <g.biscini@gmail.com>
2024-04-29 17:28:48 +0200

    prova

--- scripts/anglicismi1.js
+++ scripts/anglicismi1.js
@@ -16,30 +16,44 @@ if (document.readyState === "loading") {
     height = +svg.style("height").replace("px", "");
 
       // Add X axis
-      var x = d3.scaleLinear()
-      .domain([0, d3.max(data, function(d) { return d.numero; })])
-      .range([0, width]);
-svg.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x))
-  .selectAll("text")
-    .attr("transform", "translate(-10,0)rotate(-45)")
-    .style("text-anchor", "end");
+      d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv", function(data) {
 
-// Y axis
-var y = d3.scaleBand()
-  .range([ 0, height ])
-  .domain(data.map(function(d) { return d.anno; }))
-svg.append("g")
-  .call(d3.axisLeft(y))
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([0, 13000])
+    .range([ 0, width]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end");
 
+  // Y axis
+  var y = d3.scaleBand()
+    .range([ 0, height ])
+    .domain(data.map(function(d) { return d.Country; }))
+    .padding(.1);
+  svg.append("g")
+    .call(d3.axisLeft(y))
+
+  //Bars
   svg.selectAll("myRect")
     .data(data)
     .enter()
     .append("rect")
     .attr("x", x(0) )
-    .attr("y", function(d) { return y(d.anno); })
-    .attr("width", function(d) { return x(d.numero); })
+    .attr("y", function(d) { return y(d.Country); })
+    .attr("width", function(d) { return x(d.Value); })
     .attr("height", y.bandwidth() )
-    .attr("fill", "white")
+    .attr("fill", "#69b3a2")
+
+
+    // .attr("x", function(d) { return x(d.Country); })
+    // .attr("y", function(d) { return y(d.Value); })
+    // .attr("width", x.bandwidth())
+    // .attr("height", function(d) { return height - y(d.Value); })
+    // .attr("fill", "#69b3a2")
+
+})
   }
