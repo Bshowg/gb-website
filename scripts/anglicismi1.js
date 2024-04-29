6398d543ee8173b4ca209440a5ef9fc51cdd536e
Gianmarco Biscini <g.biscini@gmail.com>
2024-04-29 17:52:39 +0200

    test

--- scripts/anglicismi1.js
+++ scripts/anglicismi1.js
@@ -76,7 +76,6 @@ var data = {crudi:51.7,altro:48.3}
 
 // set the color scale
 var color = d3.scaleOrdinal()
-  .domain(data)
   .range(["#fff","#666"])
 
 // Compute the position of each group on the pie:
