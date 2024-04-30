93bb3abd45107ae08ef509bf740a6d9fb8217a53
Gianmarco Biscini <g.biscini@gmail.com>
2024-04-30 16:19:15 +0200

    send email

--- php/send_email.php
+++ php/send_email.php
@@ -7,33 +7,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = strip_tags(trim($_POST["message"]));
 
     // Specify the recipient email address
-    $recipient = "g.biscini@gmail.com"; // <-- Replace this with your email address
-
-    // Set the email subject
+    $recipient = "g.biscini@gmail.com"; 
     $subject = "New contact from $name";
 
-    // Build the email content in a variable
     $email_content = "Name: $name\n";
     $email_content .= "Email: $email\n\n";
     $email_content .= "Message:\n$message\n";
 
-    // Build the email headers
-    $email_headers = "From: $name <$email>";
+    // Enhanced headers
+    $email_headers = "From: $name <$email>\r\n";
+    $email_headers .= "Reply-To: $email\r\n";
+    $email_headers .= "MIME-Version: 1.0\r\n";
+    $email_headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-    // Send the email
     if (mail($recipient, $subject, $email_content, $email_headers)) {
-        // Set a 200 (okay) response code and redirect back with a success message
         http_response_code(200);
         header("Location: index.html?mailsend=success");
         exit;
     } else {
-        // Set a 500 (internal server error) response code and redirect back with an error message
         http_response_code(500);
         header("Location: index.html?mailsend=error");
         exit;
     }
 } else {
-    // Not a POST request, set a 403 (forbidden) response code.
     http_response_code(403);
     echo "There was a problem with your submission, please try again.";
 }
