0e04218e940c1bb6b77b3c2986aecc98063fd9a6
Gianmarco Biscini <gianmarco@itaca.fi.it>
2024-10-15 12:53:16 +0200

    mail sender success to static

--- php/send_email.php
+++ php/send_email.php
@@ -22,7 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (mail($recipient, $subject, $email_content, $email_headers)) {
         http_response_code(200);
-        header("Location: ../mailsendsuccess.html");
+        header("Location: ../static/mailsendsuccess.html");
         exit;
     } else {
         http_response_code(500);
