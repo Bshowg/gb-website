33a00b05730ebcc333387fccec027ce21d6f96fb
Gianmarco Biscini <gianmarco@itaca.fi.it>
2024-10-31 12:07:48 +0100

    loader sync

--- scripts/main.js
+++ scripts/main.js
@@ -32,25 +32,7 @@ function articleLoader() {
                     .then(response => response.text())
                     .then(html => {
                         const parser = new DOMParser();
-                        const doc = parser.parseFromString(html, "text/html");
-                        const title = doc.querySelector('h1') ? doc.querySelector('h1').innerText : 'No title';
-                        const excerpt = doc.querySelector('p') ? doc.querySelector('p').innerText : 'No excerpt available';
-                        const img = doc.querySelector('img') ? doc.querySelector('img').src : 'No img available';
-                        const articleHTML = `
-                            <article class="bg-black">
-
-                                <img src=${img} alt=${img} class="w-full h-auto mb-8 img_hero">
-                                <div class=" p-6">
-                                    <h3 class="text-xl font-bold mb-4 text-white">${title}</h3>
-                                    <p class="mb-4 text-white">${excerpt}</p>
-                                    <div class="flex flex-row justify-between">
-                                    <button onclick="location.href='${article.src}'" class="bg-white text-black px-3 py-1 font-bold">READ MORE</button>
-                                    <span class="align-baseline font-bold text-white">${article.date}<span>
-                                    </div>
-                                </div>
-                           </article>
-                        `;
-                        document.getElementById('articlesContainer').innerHTML += articleHTML;
+                        addArticle(parser, html, article);
                     })
                     .catch(error => {
                         console.error('Error fetching article:', error);
@@ -58,4 +40,48 @@ function articleLoader() {
             });
         }
     };
+}
+
+function articleLoaderSync() {
+    return {
+        async loadArticles() {
+            const articles = await getArticlesPhp(); // Get articles from PHP
+            console.log(articles);
+            articles.pop(1);
+            const parser = new DOMParser();
+            // Use a for...of loop to fetch each article sequentially
+            for (const article of articles) {
+                try {
+                    const response = await fetch(`articles/${article.file}`);
+                    const html = await response.text();
+                    addArticle(parser, html, article);
+                } catch (error) {
+                    console.error('Error fetching article:', error);
+                }
+            }
+        }
+    };
+}
+
+
+function addArticle(parser, html, article) {
+    const doc = parser.parseFromString(html, "text/html");
+    const title = doc.querySelector('h1') ? doc.querySelector('h1').innerText : 'No title';
+    const excerpt = doc.querySelector('p') ? doc.querySelector('p').innerText : 'No excerpt available';
+    const img = doc.querySelector('img') ? doc.querySelector('img').src : 'No img available';
+    const articleHTML = `
+                        <article class="bg-black">
+
+                            <img src=${img} alt=${img} class="w-full h-auto mb-8 img_hero">
+                            <div class=" p-6">
+                                <h3 class="text-xl font-bold mb-4 text-white">${title}</h3>
+                                <p class="mb-4 text-white">${excerpt}</p>
+                                <div class="flex flex-row justify-between">
+                                <button onclick="location.href='${article.src}'" class="bg-white text-black px-3 py-1 font-bold">READ MORE</button>
+                                <span class="align-baseline font-bold text-white">${article.date}<span>
+                                </div>
+                            </div>
+                        </article>
+                    `;
+    document.getElementById('articlesContainer').innerHTML += articleHTML;
 }
