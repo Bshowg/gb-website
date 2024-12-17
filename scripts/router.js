267b9ff0966e6424dcb0c5914139842acd22b436
Gianmarco Biscini <g.biscini@gmail.com>
2024-12-17 15:33:28 +0100

    router js no offset no limit

--- scripts/router.js
+++ scripts/router.js
@@ -1,4 +1,3 @@
-// router.js
 window.addEventListener('load', async () => {
     const navigateTo = (pathname) => {
         console.log(pathname);
@@ -6,9 +5,7 @@ window.addEventListener('load', async () => {
         router();
     };
     async function getArticlesPhp() {
-    
-        let json=await fetch("./php/get_articles.php").then(response => {
-                
+        let json = await fetch("./php/get_articles.php?limit=0&offset=0").then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
@@ -21,7 +18,7 @@ window.addEventListener('load', async () => {
         json.forEach(item => {
     
             // Construct an object with the data from the <span> elements
-            routes[item.src]=item.file;
+            routes[item.src] = item.file;
         });
     
         // Return the resultList array containing all the data objects
@@ -30,40 +27,39 @@ window.addEventListener('load', async () => {
     const routes = await getArticlesPhp();
     console.log(routes);
     const router = async () => {
-    const path = window.location.pathname;
-    var route = routes[path] || routes["/"];
-    route="articles/"+route;
+        const path = window.location.pathname;
+        var route = routes[path] || routes["/"];
+        route = "articles/" + route;
     
-    try {
-        const html = await fetch(route).then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            console.log("done")
-            return response.text();
-        });
-        document.querySelector('#app').innerHTML = html;
-        let js_path="scripts"+path+".js"
-        const js=await fetch(js_path).then(response=>{
-            if(response.ok){
-                const script = document.createElement('script');
-                script.src = `${js_path}`; //?v=${new Date().getTime()}
-                script.async = true;
-                script.onload = ()=>{console.log("content loaded");}; 
-                script.onerror = () => console.error('Error loading script:', src);
-                document.head.appendChild(script);
-            }
-        });
-        
-    } catch (error) {
-        console.error('Failed to fetch page: ', error);
-    }
-
-};
+        try {
+            const html = await fetch(route).then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                console.log("done")
+                return response.text();
+            });
+            document.querySelector('#app').innerHTML = html;
+            let js_path = "scripts" + path + ".js"
+            const js = await fetch(js_path).then(response => {
+                if (response.ok) {
+                    const script = document.createElement('script');
+                    script.src = `${js_path}`; //?v=${new Date().getTime()}
+                    script.async = true;
+                    script.onload = () => { console.log("content loaded"); };
+                    script.onerror = () => console.error('Error loading script:', src);
+                    document.head.appendChild(script);
+                }
+            });
+    
+        } catch (error) {
+            console.error('Failed to fetch page: ', error);
+        }
+    
+    };
 
     window.onpopstate = router;
     window.navigateTo = navigateTo;
 
     router();
 });
-
